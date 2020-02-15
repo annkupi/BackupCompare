5b3197764a945bd43351787d015fae5f8ef33320
annkupi <anna-kupina@yandex.ru>
2020-02-16 01:37:06 +0300

    calculate diffs and write to files

--- compare_tools/compare_backup.py
+++ compare_tools/compare_backup.py
@@ -1,7 +1,68 @@
 import utils
 
+def compare_content(data1, data2):
+    deleted = []
+    created = []
+    changed = []
+
+    id_dict1 = {item['id']: item for item in data1}
+    id_dict2 = {item['id']: item for item in data2}
+
+    ids1 = set(id_dict1.keys())
+    ids2 = set(id_dict2.keys())
+
+    deleted_ids = ids1.difference(ids2)
+    created_ids = ids2.difference(ids1)
+    other_ids = ids1.intersection(ids2)
+
+    for id in deleted_ids:
+        deleted.append({
+            'id': id,
+            'userType': id_dict1[id].get('userType')
+        })
+
+    for id in created_ids:
+        created.append({
+            'id': id,
+            'userType': id_dict1[id].get('userType')
+        })
+
+    for id in other_ids:
+        for key, value in id_dict1[id].items():
+            if value != id_dict2[id].get(key):
+                changed.append({
+                    "id": id,
+                    "attribute": key,
+                    "oldValue": value,
+                    "newValue": id_dict2[id].get(key)
+                })
+
+    return {
+        "Deleted": deleted,
+        "Added": created,
+        "ChangedAttribute": changed
+    }
+
+def save_diff(diff, path, subpath, gz_filename):
+    if subpath:
+        path += '\\' + subpath
+    utils.save_json_to_gz(diff, path, gz_filename, 'change.json')
+
 def compare(first_backup_path, second_backup_path, target_backup_path):
     # debug
-    for name in utils.all_filenames_in_path(first_backup_path):
-        utils.extract_gz_content(name)
+    first_filenames=utils.all_filenames_in_path(first_backup_path)
+    second_filenames=utils.all_filenames_in_path(second_backup_path)
+    for first_subpath, first_filename in first_filenames:
+        second_subpath, second_filename = next(second_filenames)
 
+        #debug, delete
+        print(first_filename)
+        print(second_filename)
+
+        first_content = utils.extract_gz_content(
+            utils.make_full_path(first_backup_path, first_subpath, first_filename)).get('value')
+        second_content = utils.extract_gz_content(
+            utils.make_full_path(second_backup_path, second_subpath, second_filename)).get('value')
+
+        diff = compare_content(first_content, second_content)
+        save_diff(diff, target_backup_path, first_subpath, first_filename)
--- compare_tools/utils.py
+++ compare_tools/utils.py
@@ -1,15 +1,30 @@
 import gzip
 import json
-from os import walk
+import os
+import shutil
 
 def all_filenames_in_path(path):
-    for (dirpath, dirnames, filenames) in walk(path):
-        names_with_path = [dirpath + '\\' + filename for filename in filenames]
-        for item in names_with_path:
-            yield item
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for filename in filenames:
+            yield dirpath[len(path) + 1:], filename
+
+def make_full_path(path, subpath, file_name):
+    return '{}\\{}\\{}'.format(path, subpath, file_name)
 
 def extract_gz_content(path):
-    file = gzip.open(path, 'rb')
-    file_content_dict = json.load(file)
-    file.close()
+    with gzip.open(path, 'rb') as f:
+        file_content_dict = json.load(f)
     return file_content_dict
+
+def save_json_to_gz(data, path, gz_filename, filename):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    internal = open(path + '\\' + filename, 'wb')
+    json.dump(data, internal)
+    internal.close()
+    internal = open(path + '\\' + filename, 'rb')
+    with gzip.open(path + '\\' + gz_filename, 'wb') as f:
+        shutil.copyfileobj(internal, f)
+    internal.close()
+    os.remove(path + '\\' + filename)
